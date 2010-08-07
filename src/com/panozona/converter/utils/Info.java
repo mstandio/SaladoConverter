95edf43efffe2ec8c01282ff23d2936115420d84
mstandio <mstandio@o2.pl>
2010-08-08 01:29:46 +0200

    Learning github ;]

---
+++ src/com/panozona/converter/utils/Info.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.panozona.converter.utils;
+
+/**
+ *
+ * @author Marek
+ */
+public class Info {
+
+    public final static String EC_OVERLAP_ERROR = "Invalid value for Overlap in EquirectangularToCubic tab";
+    public final static String EC_INTERPOLATION_ERROR = "Invalid value for Interpolation in EquirectangularToCubic tab";
+    public final static String EC_QUALITY_ERROR = "Invalid value for Quality in EquirectangularToCubic tab";
+    public final static String EC_VERBOSE_ERROR = "Invalid value for Verbose in EquirectangularToCubic tab";
+    public final static String EC_JAR_DIR_ERROR = "Invalid value for jar directory in EquirectangularToCubic tab";
+    public final static String DZT_OVERLAP_ERROR = "Invalid value for Overlap in DeepZoomTiler tab";
+    public final static String DZT_TILESIZE_ERROR = "Invalid value for Tile size in DeepZoomTiler tab";
+    public final static String DZT_QUALITY_ERROR = "Invalid value for Quality in DeepZoomTiler tab";
+    public final static String DZT_VERBOSE_ERROR = "Invalid value for Verbose in DeepZoomTiler tab";
+    public final static String DZT_JAR_DIR_ERROR = "Invalid value for jar directory in DeepZoomTiler tab";
+    public final static String GE_SEARCH_SUB_ERROR = "Invalid value for sear in General tab";
+    public final static String GE_DEPTH_ERROR = "Invalid value for depth in General tab";
+    public final static String GE_TMP_DIR_ERROR = "Invalid value for tmp directory in General tab";
+    public final static String GE_OUT_DIR_ERROR = "Invalid value for output directory";
+    public final static String GE_IN_DIR_ERROR = "Invalid value for input directory";
+    public final static String GE_COMMAND_ERROR = "Invalid command";
+
+
+}
